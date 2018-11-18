de7178c8e16a418ea9177f335636cf90569692e0
KevinR777 <krod995@gmail.com>
2018-11-17 19:09:51 -0600

    Realizando pruebas sobre imagenes propias

--- test_model.py
+++ test_model.py
@@ -1,24 +1,43 @@
-# Importing the Keras libraries and packages
-from keras.models import load_model
+# Librerias necesarias
+import tensorflow as tf
+from tensorflow import keras
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
 from PIL import Image
 import numpy as np
-import sys
 
-def testModel(modelName , directory):
 
-	model = load_model(modelName)
-	for index in range(10):
-	    img = Image.open(directory+'/' + str(index) + '.png').convert("L")
-	    img = img.resize((28,28))
-	    im2arr = np.array(img)
-	    im2arr = im2arr.reshape(1,28,28,1)
-	    # Predicting the Test set results
-	    y_pred = model.predict(im2arr)
-	    print(y_pred)
+#Inicializamos el modelo en nulo
+new_model = None
 
+
+#Funcion: loadModel
+#Se encarga de cargar el modelo o red neuronal especificada
+def loadModel(modelName):
+	global new_model
+	new_model = tf.keras.models.load_model(modelName + '.h5')
+
+
+def testModel(image_name):
+	global new_model
+	img = Image.open('data/' + str(image_name)).convert("L")
+	img.show()
+	img = img.resize((28,28))
+	im2arr = np.array(img)
+	im2arr = im2arr.reshape(1,28,28)
+	# Predicting the Test set results
+	predictions = new_model.predict(im2arr)
+	print(np.argmax(predictions))
+
+
+#Funcion: main()
+#Carga el modelo y lo prueba
 def main():
 	modelName = sys.argv[1]
-	directory = sys.argv[2]
-	testModel(modelName, directory)
+	image_name = sys.argv[2]
+	loadModel(modelName)
+	testModel(image_name)
+
 
 main()
